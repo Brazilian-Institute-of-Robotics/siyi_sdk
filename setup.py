941000388c31538d98f32c4f6378a75bbc9f43b0
Felipe Nascimento <46880061+rnfelipe@users.noreply.github.com>
2026-06-10 17:16:12 -0300

    Handle error when not founding README.md file

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import find_packages, setup
+import os
+
+try:
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = "Python package implementation for Siyi SDK."
 
 setup(
     name="siyi_sdk",
     version="0.1.0",
     description="Python package implementation for Siyi SDK.",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Mohamed Abdelkader",
     author_email="mohamedashraf123@gmail.com",
